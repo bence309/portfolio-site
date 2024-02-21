9f79146515dfd842df2ea2b5559afbaf837b65f9
bence309 <bence309@hotmail.com>
2024-02-21 10:41:12 +0100

    AWS S3 bucket can be created by Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,24 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "eu-north-1" 
 }
 
 resource "aws_s3_bucket" "portfolio_bucket" {
   bucket = "portfolio-terraform-bence-bodi"
-  acl    = "public-read"
+  force_destroy = true  # Ensure that the bucket can be destroyed even if it's not empty
 
   website {
     index_document = "index.html"
   }
 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "Statement1",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::portfolio-terraform-bence-bodi/*"
-        }
-    ]
-}
-POLICY
+  tags = {
+    Name = "PortfolioWebsiteBucket"
+  }
 }
 
 resource "aws_s3_bucket_object" "portfolio_website_files" {
   bucket = aws_s3_bucket.portfolio_bucket.bucket
   key    = "build/"
-  source = "../build/"
-  recursive = true
-  acl    = "public-read"
+  for_each = fileset("../build", "**/*")
+
+  source = "../build/${each.value}"
 }
